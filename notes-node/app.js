2410ab0d49171e944b0790d27282b68956b3bf77
Courtney Ngai <ctngai@asu.edu>
2018-07-08 21:59:14 -0700

    Removed files

--- notes-node/app.js
+++
@@ -1,8 +0,0 @@
-console.log('Starting app.js');
-
-const fs = require('fs');
-const os = require('os');
-
-var user = os.userInfo();
-
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
